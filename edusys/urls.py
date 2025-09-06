a6ae9e9304c3132cf80661da2f2e4cb41d2acc6c
Fomalhaut <364639629@qq.com>
2025-09-06 18:39:30 +0800

    update some pages

--- edusys/urls.py
+++ edusys/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("students/", include("students.urls")),
+    path("users/", include("users.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def register(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		password2 = request.POST.get('password2')
+		if password != password2:
+			messages.error(request, '两次密码不一致！')
+			return render(request, 'users/register.html')
+		if User.objects.filter(username=username).exists():
+			messages.error(request, '用户名已存在！')
+			return render(request, 'users/register.html')
+		User.objects.create_user(username=username, password=password)
+		messages.success(request, '注册成功，请登录！')
+		return redirect('login')
+	return render(request, 'users/register.html')
+
+def login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			auth_login(request, user)
+			return redirect('/')
+		else:
+			messages.error(request, '用户名或密码错误！')
+	return render(request, 'users/login.html')
